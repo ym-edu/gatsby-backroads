45a5dab63b04e486e13b4118c53663942981c2ff
nym <nym@nym.local>
2020-12-17 16:02:56 -0600

    created link tests from home to blog and vice versa

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,11 @@
+import { Link } from 'gatsby'
 import React from 'react'
 
 export default function blog() {
   return (
     <div>
       hello from blog page
+      <Link to="/">Home</Link>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import {Link} from 'gatsby'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return(
+  <div>
+    Hello world!
+    <Link to="/blog/">blog page</Link>
+  </div>
+  )
 }
