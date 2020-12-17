ab64f7fbf4c1576c945f9891c644e2195a4c2d48
nym <nym@nym.local>
2020-12-17 17:28:30 -0600

    wrapped all pages in layout to render shared layout components

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,12 @@
 import { Link } from 'gatsby'
 import React from 'react'
+import Layout from '../components/Layout'
 
 export default function blog() {
   return (
-    <div>
+     <Layout>
       hello from blog page
       <Link to="/">Home</Link>
-    </div>
+     </Layout>
   )
 }
