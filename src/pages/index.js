6d6aeacb1bbf72e7afd2ffa0b8f59ce54bc9abf7
nym <nym@nym.local>
2020-12-19 14:45:09 -0600

    installed react components; tested one in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import React from "react"
 import {Link} from 'gatsby'
 import Layout from '../components/Layout'
+import { MdSpa } from "react-icons/md"
 
 export default function Home() {
   return(
   <>
     <Layout>
       Hello world!
-      <Link to="/blog/">blog page</Link>
+      <Link to="/blog/">blog page <MdSpa/></Link>
     </Layout>
   </>
   )
