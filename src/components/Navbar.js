eed3d0961eec7cc8ae5deeea1526455155f6bbf0
nym <nym@nym.local>
2020-12-19 17:16:34 -0600

    Set up jsx and linked appropriate css; mapped out page/social links from constants data

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import {Link} from 'gatsby'
 import styles from '../css/navbar.module.css'
 import {FaAlignRight} from 'react-icons/fa'
 import links from '../constants/links'
@@ -8,12 +9,39 @@ import logo from '../images/logo.svg'
 export default function Navbar() {
   const [toggleNav,setToggleNav] = useState(false);
   const toggle = () => {
-    setToggleNav(prev => !toggleNav)
+    setToggleNav((prev) => !prev)
   }
 
   return (
-    <React.Fragment>
-      <h1>I am Navbar</h1>
-    </React.Fragment>
+    <nav className={styles.navbar}>
+      <div className={styles.navCenter}>
+        <div className={styles.navHeader}>
+          <img src={logo} alt="backroads logo"/>
+          <button type="button" className={styles.logoBtn} onClick={toggle}>
+            <FaAlignRight className={styles.logoIcon} />
+          </button>
+        </div>
+        <ul className={toggleNav ?
+          `${styles.navLinks} ${styles.showNav}` :
+          `${styles.navLinks}`}>
+            {links.map((item, index) => {
+              return (
+              <li key={index}>
+                <Link to={item.path}>{item.text}</Link>
+              </li>
+              )
+            })}
+        </ul>
+        <div className={styles.navSocialLinks}>
+            {socialLinks.map((item, index) => {
+              return (
+                <a key={index} href={item.url} target="_blank" rel="noreferrer">
+                  {item.icon}
+                </a>
+              )
+            })}
+        </div>
+      </div>
+    </nav>
   )
 }
