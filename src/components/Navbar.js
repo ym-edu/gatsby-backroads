b9a930b91f97705135199e3b981ab98f18ac959e
nym <nym@nym.local>
2020-12-19 15:32:26 -0600

    importing nav-css-module from css folder copied from setup-files; importing icons from react-icons; importing link data from constant files; importing svg log from images; defined toggle state variable for toggle functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
+import styles from '../css/navbar.module.css'
+import {FaAlignRight} from 'react-icons/fa'
+import links from '../constants/links'
+import socialLinks from '../constants/social-links'
+import logo from '../images/logo.svg'
 
 export default function Navbar() {
+  const [toggleNav,setToggleNav] = useState(false);
+  const toggle = () => {
+    setToggleNav(prev => !toggleNav)
+  }
+
   return (
     <React.Fragment>
       <h1>I am Navbar</h1>
