b5caf5fef2f93481272e89b5339a26db4fc315a8
nym <nym@nym.local>
2020-12-19 14:55:47 -0600

    set up constant files from which to pull data from for Links and SocialLinks components

---
+++ src/constants/social-links.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {FaFacebook, FaTwitterSquare,FaSquarespace} from 'react-icons/fa'
+
+export default [
+  {
+    icon:<FaFacebook></FaFacebook>,
+    url:'https://twitter.com'
+  },
+  {
+    icon:<FaTwitterSquare></FaTwitterSquare>,
+    url:'#'
+  },
+  {
+    icon:<FaSquarespace></FaSquarespace>,
+    url:'#'
+  },
+]
