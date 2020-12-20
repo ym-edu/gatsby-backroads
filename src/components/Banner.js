964b9ebf7c3115579cbcb6c94df3cae6c854e304
nym <nym@nym.local>
2020-12-20 15:58:24 -0600

    Created a banner component that renders an h1 and p and takes children; h1 and p content value is passed in as prop defined in Index page where it is being called within the SimpleHero component

---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../css/banner.module.css'
+
+export default function Banner({title,info,children}) {
+  return (
+    <div className={styles.banner}>
+      <h1>{title}</h1>
+      <p>{info}</p>
+      {children}
+    </div>
+  )
+}
