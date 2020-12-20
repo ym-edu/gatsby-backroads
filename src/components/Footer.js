0c663ff594ecf748303636025bef4c6cf2adbda0
nym <nym@nym.local>
2020-12-20 12:46:30 -0600

    Set up jsx and linked appropriate css; mapped out page/social links from constants data; added copyright with dynamic year-date

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import styles from '../css/footer.module.css'
+import links from '../constants/links'
+import socialLinks from '../constants/social-links'
+import {Link} from 'gatsby'
+import {FaRProject} from 'react-icons/fa'
 
 export default function Footer() {
   return (
-    <>
-      <h1>I am Footer</h1>
-    </>
+    <footer className={styles.footer}>
+      <ul className={styles.links}>
+        {links.map((item, index) => {
+          return <Link to={item.path} key={index}>{item.text}</Link>
+        })}
+      </ul>
+      <div className={styles.icons}>
+        {socialLinks.map((item, index) => {
+          return <Link to={item.url} key={index}>{item.icon}</Link>
+        })}
+      </div>
+      <div className={styles.copyright}>
+        copyright &#169; backroads travel company {new Date().getFullYear()}. all rights reserved
+      </div>
+    </footer>
   )
 }
