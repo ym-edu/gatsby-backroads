bf947a57c669d48a86b75b1ae670cf1c2eed47c8
nym <nym@nym.local>
2020-12-20 13:20:28 -0600

    Created a hero component using unoptimized method of setting background image through css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,12 @@
 import React from "react"
-import {Link} from 'gatsby'
 import Layout from '../components/Layout'
-import { MdSpa } from "react-icons/md"
+import SimpleHero from "../components/SimpleHero"
 
 export default function Home() {
   return(
   <>
     <Layout>
-      Hello world!
-      <Link to="/blog/">blog page <MdSpa/></Link>
+      <SimpleHero></SimpleHero>
     </Layout>
   </>
   )
